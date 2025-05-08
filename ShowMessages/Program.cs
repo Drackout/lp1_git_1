63d5550992291677542d816be6b5723ff00cfb68
Drackout <mikatx2@hotmail.com>
2025-05-09 00:38:40 +0100

    mylp1branch stuff

---
+++ ShowMessages/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ShowMessages
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            //mess
+            //
+            //kawabanga
+            Console.WriteLine("Hello LP1!");
+        }
+    }
+}
